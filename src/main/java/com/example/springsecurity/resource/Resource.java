b383ef6cc6d2670a3c77dfae25eecdbf41b9843d
mainakdas195 <65386328+mainakdas195@users.noreply.github.com>
2021-12-15 17:21:47 +0530

    Add files via upload
    commit

--- src/main/java/com/example/springsecurity/resource/Resource.java
+++ src/main/java/com/example/springsecurity/resource/Resource.java
@@ -6,17 +6,17 @@ import org.springframework.web.bind.annotation.GetMapping;
 
 public interface Resource {
 	@GetMapping("/findAll")
-	@PreAuthorize("hasRole('ROLE_ADMIN') or hasRole('ROLE_USER')")
+	@PreAuthorize("hasRole('ROLE_ADMIN_US') or hasRole('ROLE_USER_UK')")
 	ResponseEntity<String> findAll();
 	
 	@GetMapping("{id}")
 	ResponseEntity<String> findById();
 	
 	@GetMapping("/update")
-	@PreAuthorize("hasRole('ROLE_ADMIN')")
+	@PreAuthorize("hasRole('ROLE_ADMIN_UK')")
 	ResponseEntity<String> update();
 	
 	@GetMapping("/invalid")
-	@PreAuthorize("hasRole('ROLE_USER')")
+	@PreAuthorize("hasRole('ROLE_USER_US')")
 	ResponseEntity<String> invalid();
 }
